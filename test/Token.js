a05f9ad63fcd4326f2b1f686ac79dc7b81a36ede
Yash Patel <ypatel5400@gmail.com>
2021-06-30 21:25:20 -0400

    Add test

--- test/Token.js
+++ test/Token.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 
 describe("Token contract", function() {
     it("Deployment should assign total to deployer", async function() {
-        const [owner] = await ethers.getSigners();
+        const [owner, addr1, addr2] = await ethers.getSigners();
 
         const Token = await ethers.getContractFactory("Token");
 
@@ -10,5 +10,11 @@ describe("Token contract", function() {
 
         const ownerBalance = await hardhatToken.balanceOf(owner.address);
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+
+        await hardhatToken.transfer(addr1.address, 50);
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+        
+        await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);     
     });
 });
